b3a7f22565766e76cf8aeb2eaf339e39d1e7293b
alfremedpal <alfremedpal@gmail.com>
2020-08-24 13:22:10 -0500

    Hero banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,8 +5,20 @@ export default function Home() {
 
     return (
         <>
-            <div className={[classes.hero, "pattern-cross-dots-lg "].join(' ')}>
-                Home
+            <div className={[classes.hero, "pattern-cross-dots-lg"].join(' ')}>
+                <div className={classes.spacer}/>
+                <div className={classes.title}>
+                    <h1>Keep track of the words you stutter with</h1>
+                    <p>
+                        Get to know yourself a little bit better by 
+                        generating reports about the words you have 
+                        trouble with and the situation in which they 
+                        occur.
+                    </p>
+                </div>
+            </div>
+            <div className={classes.section}>
+                Section
             </div>
         </>
     )
